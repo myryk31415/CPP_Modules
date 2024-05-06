49acfb2186671684861575ae8bbf6bdf1904ffd8
Myryk <myryk@posteo.de>
2024-05-06 02:03:41 +0200

    01ex05 started

---
+++ 01/ex05/include/harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 01:25:36 by padam             #+#    #+#             */
+/*   Updated: 2024/05/06 01:28:12 by padam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class	Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl(void);
+		~Harl(void);
+		void	complain( std::string level );
+};
+
+#endif

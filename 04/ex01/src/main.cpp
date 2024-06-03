70c735faa46f20d02823bcd2a01a2c425e0f1560
Myryk <myryk@posteo.de>
2024-06-03 22:50:05 +0200

    removed wrongAnimal

--- 04/ex01/src/main.cpp
+++ 04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 21:33:55 by padam             #+#    #+#             */
-/*   Updated: 2024/06/03 15:28:08 by padam            ###   ########.fr       */
+/*   Updated: 2024/06/03 22:49:23 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,18 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 #include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
+##include "Brain.hpp"
 
 int main()
 {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	const WrongAnimal* wrong = new WrongAnimal();
-	const WrongAnimal* wrongCat = new WrongCat();
 
 	std::cout << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	std::cout << meta->getType() << " " << std::endl;
-	std::cout << wrong->getType() << " " << std::endl;
-	std::cout << wrongCat->getType() << " " << std::endl;
 
 	std::cout << std::endl;
 	i->makeSound(); //will output the cat sound!
@@ -37,14 +33,8 @@ int main()
 	meta->makeSound();
 
 	std::cout << std::endl;
-	wrong->makeSound();
-	wrongCat->makeSound();
-
-	std::cout << std::endl;
 	delete meta;
 	delete j;
 	delete i;
-	delete wrong;
-	delete wrongCat;
 	return 0;
 }

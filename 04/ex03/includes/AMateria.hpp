3f00bbf0b5348d9bbe2db2eadd64105a14d457b9
Myryk <myryk@posteo.de>
2024-06-04 01:41:16 +0200

    04ex03 changes

--- 04/ex03/includes/AMateria.hpp
+++ 04/ex03/includes/AMateria.hpp
@@ -10,6 +10,15 @@ class AMateria
 		AMateria &operator=(const AMateria &other);
 		~AMateria();
 };
+class AMateria
+{
+	protected:
+	public:
+		AMateria(std::string const & type);
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
 
 #endif
 

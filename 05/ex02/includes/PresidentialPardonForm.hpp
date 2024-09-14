ae7731db47a6ac6ef1c21a3ae46c47314521cae5
Myryk <myryk@posteo.de>
2024-09-14 09:24:34 +0200

    05ex02 added presidential pardon

---
+++ 05/ex02/includes/PresidentialPardonForm.hpp
@@ -0,0 +1,19 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+# include <iostream>
+# include "Form.hpp"
+
+class PresidentialPardonForm : public Form
+{
+	private:
+		void	_execute(void) const;
+	public:
+		PresidentialPardonForm(void);
+		PresidentialPardonForm(std::string target);
+		PresidentialPardonForm(const PresidentialPardonForm& other);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
+		~PresidentialPardonForm();
+};
+
+#endif
+
---
+++ 05/ex02/src/PresidentialPardonForm.cpp
@@ -0,0 +1,42 @@
+#include "PresidentialPardonForm.hpp"
+
+// Default constructor
+PresidentialPardonForm::PresidentialPardonForm(void) : Form("PresidentialPardonForm", "default_target", 25, 5)
+{
+	// std::cout << "PresidentialPardonForm Default constructor called" << std::endl;
+	return;
+}
+
+// Input constructor
+PresidentialPardonForm::PresidentialPardonForm(std::string target) : Form("PresidentialPardonForm", target, 25, 5)
+{
+	// std::cout << "PresidentialPardonForm Input constructor called" << std::endl;
+	return;
+}
+
+// Copy constructor
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other) : Form(other)
+{
+	// std::cout << "PresidentialPardonForm Copy constructor called" << std::endl;
+	return;
+}
+
+// Assignment operator overload
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &other)
+{
+	// std::cout << "PresidentialPardonForm Assignment operator called" << std::endl;
+	(void)other;
+	return (*this);
+}
+
+// Destructor
+PresidentialPardonForm::~PresidentialPardonForm(void)
+{
+	// std::cout << "PresidentialPardonForm Destructor called" << std::endl;
+	return;
+}
+
+void	PresidentialPardonForm::_execute() const
+{
+	std::cout <<this->get_target() << " has been pardoned by Rick Astley ;)\n" << std::endl;
+}

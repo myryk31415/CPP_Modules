a181f6d2475573b9edfee1428d104a90e6b02fef
Myryk <myryk@posteo.de>
2024-09-14 09:24:10 +0200

    05ex02 added robotomy

---
+++ 05/ex02/includes/RobotomyRequestForm.hpp
@@ -0,0 +1,21 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+# include <iostream>
+# include "Form.hpp"
+# include <cstdlib>
+# include <ctime>
+
+class RobotomyRequestForm : public Form
+{
+	private:
+		void	_execute(void) const;
+	public:
+		RobotomyRequestForm(void);
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm& other);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &other);
+		~RobotomyRequestForm();
+};
+
+#endif
+

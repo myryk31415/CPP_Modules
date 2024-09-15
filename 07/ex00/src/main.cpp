866ff583ef6be128055924760ca685efe37f21f8
Myryk <myryk@posteo.de>
2024-09-15 19:54:15 +0200

    07ex00 done

---
+++ 07/ex00/src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
+
+// int	main(void)
+// {
+// 	int a = 5;
+// 	int b = 7;
+
+// 	std::cout << "a: " << a << std::endl;
+// 	std::cout << "b: " << b << std::endl;
+
+// 	swap(a, b);
+// 	std::cout << "Swapped a and b!" << std::endl;
+
+// 	std::cout << "a: " << a << std::endl;
+// 	std::cout << "b: " << b << std::endl;
+
+// 	std::cout << "min: " << min(a, b) << std::endl;
+// 	std::cout << "max: " << max(a, b) << std::endl;
+
+// }

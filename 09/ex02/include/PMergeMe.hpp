91ea76b29c6e294e44068aba172b0435b177d9e7
Myryk <myryk@posteo.de>
2024-11-27 15:01:09 +0100

    09ex02 added Pair class

--- 09/ex02/include/PMergeMe.hpp
+++ 09/ex02/include/PMergeMe.hpp
@@ -1,18 +1,40 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 # include <iostream>
+# include <vector>
+# include <deque>
 
-template <typename type>
 class PMergeMe
 {
-	private:
-		typedef std::vector<std::pair<type::value_type, type::value_type>> type_arr;
 	public:
 		PMergeMe(void);
 		PMergeMe();
 		PMergeMe(const PMergeMe& other);
 		PMergeMe &operator=(const PMergeMe &other);
 		~PMergeMe();
+	private:
+		class Pair
+		{
+			public:
+				Pair(void);
+				Pair(int first, int second);
+				Pair(const Pair& other);
+				Pair &operator=(const Pair &other);
+				~Pair(void);
+
+				int		get_first_int(void) const;
+				int		get_second_int(void) const;
+				Pair	*get_first_pair(void);
+				Pair	*get_second_pair(void);
+			private:
+				bool	last;
+				bool	single;
+				Pair	*first_pair;
+				Pair	*second_pair;
+				int		first_int;
+				int		second_int;
+		};
+		std::vector<Pair>	pair_list;
 };
 
 #endif

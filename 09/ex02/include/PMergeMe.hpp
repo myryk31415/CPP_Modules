64861a3932936ecb104fb0961a69e3f15a42ff6d
Myryk <myryk@posteo.de>
2024-11-30 13:49:27 +0100

    removed unecessary code

--- 09/ex02/include/PMergeMe.hpp
+++ 09/ex02/include/PMergeMe.hpp
@@ -13,28 +13,8 @@ class PMergeMe
 		PMergeMe &operator=(const PMergeMe &other);
 		~PMergeMe();
 	private:
-		class Pair
-		{
-			public:
-				Pair(void);
-				Pair(int first, int second);
-				Pair(const Pair& other);
-				Pair &operator=(const Pair &other);
-				~Pair(void);
-
-				int		get_first_int(void) const;
-				int		get_second_int(void) const;
-				Pair	*get_first_pair(void);
-				Pair	*get_second_pair(void);
-			private:
-				bool	last;
-				bool	single;
-				Pair	*first_pair;
-				Pair	*second_pair;
-				int		first_int;
-				int		second_int;
-		};
-		std::vector<Pair>	pair_list;
+		std::vector<std::pair<int, int>	pair_vector;
+		std::deque<std::pair<int, int>	pair_deque;
 };
 
 #endif
